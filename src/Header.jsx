a8504fdc991abc619d5a4dacedffae7da7ea835b
Annyotamo Barman <abprogresss@gmail.com>
2024-02-27 17:18:58 +0530

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
 import styles from "./css/Header.module.css";
-import headerImg from "./assets/headerImg.png";
+import logo from "./assets/logo.png";
 export default function Header() {
     return (
         <div className={styles["header-container"]}>
-            <img src={headerImg} alt="Project logo" />
+            <img src={logo} alt="Project logo" />
         </div>
     );
 }
